e50eadae96d47cb42d1259191b9f880351541a7d
Pinel <raoulpinel192@gmail.com>
2025-09-09 15:58:43 +0200

    genMV.sh

--- genMV.sh
+++ genMV.sh
@@ -2,17 +2,26 @@
 
 set -x
 
-VM_NAME="Debian1"
+VM_NAME="Debian46"
 RAM=4096
 OS_TYPE="Debian_64"
 
 VM_PATH="$HOME/VirtualBox VMs/$VM_NAME"
 
-VBoxManage createvm --name "$VM_NAME" --ostype "$OS_TYPE" --register
+VBoxManage createvm --name "$VM_NAME"
 
-VBoxManage modifyvm "$VM_NAME" \
-  --memory "$RAM"
+samy="$?"
 
-#VBoxManage unregistervm "$VM_NAME" --delete
+if [ "$samy" != 0 ]; then
+	echo " mais non jeune homme"
+else
+	echo " bravo jeune homme"
+fi
+
+VBoxManage registervm "$VM_PATH/$VM_NAME.vbox"
+
+VBoxManage modifyvm "$VM_NAME" --memory "$RAM"
+
+VBoxManage unregistervm "$VM_NAME" --delete
 
 # rajouter input, gestion d'erreurs, rajoouter les arguments
